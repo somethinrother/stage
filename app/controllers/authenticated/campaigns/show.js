aa4b666f5c8e8f841e45eb8bbdb51a49cfefdb19
somethinrother <jamesneilson4@gmail.com>
2019-07-25 14:59:47 -0400

    Links can be displayed dynamically

--- app/controllers/authenticated/campaigns/show.js
+++ app/controllers/authenticated/campaigns/show.js
@@ -3,6 +3,8 @@ import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
 import { computed } from '@ember/object';
 
+const PATH_NAMES = ['overview', 'chapters', 'locations', 'party', 'blue-booking', 'characters'];
+
 export default Controller.extend({
   currentUser: service('current-user'),
   campaign: alias('model'),
@@ -12,5 +14,8 @@ export default Controller.extend({
   currentUserId: alias('currentUser.user.id'),
   currentUserIsGm: computed('campaign', function() {
     return this.ownerId === this.currentUserId;
+  }),
+  links: computed(function() {
+    return PATH_NAMES;
   })
 });
