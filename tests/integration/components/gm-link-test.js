667e854179a09f200091bf09a8b5223f17b36af9
somethinrother <jamesneilson4@gmail.com>
2019-07-27 14:31:55 -0400

    One of the gm link tests passes

--- tests/integration/components/gm-link-test.js
+++ tests/integration/components/gm-link-test.js
@@ -2,13 +2,31 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { newSession } from 'stage/tests/helpers/sessions/sign-in';
+
+let user, campaign;
 
 module('Integration | Component | gm-link', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(async function() {
+    user = await newSession(this);
+    campaign = server.create('campaign', { user });
+    this.set('campaign', campaign);
+    this.owner.setupRouter();
+  });
 
   test('it displays if currentUser is the GM', async function(assert) {
+    await render(hbs`<GmLink @campaign={{campaign}} />`);
+    assert.dom('[data-test-gm-link="campaign-edit"]').exists();
   });
 
   test('it does not display if currentUser is not the GM', async function(assert) {
+    await render(hbs`<GmLink @campaign={{campaign}} />`);
+    await render(hbs`<div data-test-div='test'/>`);
+    assert.dom('[data-test-div="test"]').exists();
+    assert.dom('[data-test-gm-link="campaign-edit"]').doesNotExist();
   });
 });
