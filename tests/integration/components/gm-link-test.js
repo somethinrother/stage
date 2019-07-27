93ffc9c07974623b33e3a168380d594c59d8ad15
somethinrother <jamesneilson4@gmail.com>
2019-07-27 14:16:46 -0400

    Get gm-link working, write a couple failing tests

---
+++ tests/integration/components/gm-link-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | gm-link', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it displays if currentUser is the GM', async function(assert) {
+  });
+
+  test('it does not display if currentUser is not the GM', async function(assert) {
+  });
+});
