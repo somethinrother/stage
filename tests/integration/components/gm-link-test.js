241d1590b6cd80b4cb1359aad65201c1087dd43b
somethinrother <jamesneilson4@gmail.com>
2019-07-27 14:42:38 -0400

    GM link tests pass

--- tests/integration/components/gm-link-test.js
+++ tests/integration/components/gm-link-test.js
@@ -5,7 +5,7 @@ import hbs from 'htmlbars-inline-precompile';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { newSession } from 'stage/tests/helpers/sessions/sign-in';
 
-let user, campaign;
+let user;
 
 module('Integration | Component | gm-link', function(hooks) {
   setupRenderingTest(hooks);
@@ -13,19 +13,24 @@ module('Integration | Component | gm-link', function(hooks) {
 
   hooks.beforeEach(async function() {
     user = await newSession(this);
-    campaign = server.create('campaign', { user });
-    this.set('campaign', campaign);
     this.owner.setupRouter();
+    const currentUser = this.owner.factoryFor('service:current-user').create();
+    this.set('currentUser', currentUser);
   });
 
   test('it displays if currentUser is the GM', async function(assert) {
+    server.create('campaign', { user });
     await render(hbs`<GmLink @campaign={{campaign}} />`);
+
     assert.dom('[data-test-gm-link="campaign-edit"]').exists();
   });
 
   test('it does not display if currentUser is not the GM', async function(assert) {
+    const campaign = server.create('campaign');
+    this.set('campaign', campaign);
     await render(hbs`<GmLink @campaign={{campaign}} />`);
     await render(hbs`<div data-test-div='test'/>`);
+
     assert.dom('[data-test-div="test"]').exists();
     assert.dom('[data-test-gm-link="campaign-edit"]').doesNotExist();
   });
